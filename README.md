0793a6c5f5e2f38a4df167561ca854562115ddc1
briheet <briheetyadav@gmail.com>
2024-11-12 17:40:44 +0530

    compress for png done

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# Png
+# Image
 
-This contains some go methods to drive on png.
+This package provides Go methods to handle image file operations, specifically for compressing PNG images.
+Currently only works with .png files.
+
+### Prerequisites
+
+This tool uses [pngquant](https://pngquant.org/) for image compression. Please make sure to install `pngquant` to ensure the tool works correctly.
+
+### Usage
+
+Run the executable with the following commands and arguments:
+
+#### Commands
+
+- **Help**  
+  Display help information and usage examples.
+
+  ```bash
+  ./binary -h
+  ```
+
+- **Compress**  
+  Compress a PNG file to reduce its size while retaining quality.
+
+  ```bash
+  ./binary -compress <SourceFile.png> <OutputFile.png>
+  ```
--- main.go
+++ main.go
@@ -7,16 +7,53 @@ import (
 	"github.com/briheet/gopng/compress"
 )
 
-func main() {
-	fmt.Println("starting...")
-
-	compressedFile, err := compress.CompressToFile("Firefox_wallpaper.png", "compress.png")
+func compressTheGivenFile(given string, got string) os.File {
+	fmt.Println("wait babygurl, its compressing :)")
+	compressedFile, err := compress.CompressToFile(given, got)
 	if err != nil {
 		panic(err)
 	}
 	defer compressedFile.Close()
 
-	fmt.Println("...ending")
+	return *compressedFile
+}
 
+func main() {
+	fmt.Println("starting...")
+	fmt.Println("Pass in the arguments, enter -h for help")
+
+	arguments := os.Args
+
+	if len(arguments) < 2 {
+		fmt.Println("Error: Insufficient arguments.")
+		fmt.Println("Usage:")
+		fmt.Println("  -h for help")
+		os.Exit(-1)
+	}
+
+	switch arguments[1] {
+	case "-h":
+		fmt.Println("Help:")
+		fmt.Println("Usage:")
+		fmt.Println("  -compress GivenFileName OutputFileName")
+		os.Exit(0)
+
+	case "-compress":
+		if len(arguments) != 4 {
+			fmt.Println("Error: Incorrect number of arguments for compression.")
+			fmt.Println("Usage: -compress GivenFileName OutputFileName")
+			os.Exit(-1)
+		}
+		compressedFile := compressTheGivenFile(arguments[2], arguments[3])
+		defer compressedFile.Close()
+
+	default:
+		fmt.Println("Error: Unknown command.")
+		fmt.Println("Usage:")
+		fmt.Println("  -h for help")
+		os.Exit(-1)
+	}
+
+	fmt.Println("...ending")
 	os.Exit(0)
 }
